eb45fa8a9f825806d1997e8b1e98b36fa95285f3
rimuyamaura <148062753+rimuyamaura@users.noreply.github.com>
2024-08-03 18:33:48 +1200

    switch to Azure SQL DB

---
+++ job-log.Server/Core/Data/Migrations/20240803055117_newDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace job_log.Server.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
